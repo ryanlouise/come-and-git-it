1f1e425c8587478e5847b484c883abe99a9464dd
Ryan Epis <epis.181221@naga.sti.edu.ph>
2021-11-01 01:38:39 +0800

    added subtraction feature

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
-    cout << "hello i am ryan";
+    int firstNumber, secondNumber, sumOfTwoNumbers, diff;
+    cout << "hello i am ryan" << endl;
+
     cout << "Enter two integers: ";
     cin >> firstNumber >> secondNumber;
 
-    // sum of two numbers in stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
+    diff = firstNumber - secondNumber;
 
     // Prints sum 
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers << endl;     
+    cout << firstNumber << " - " <<  secondNumber << " = " << diff;     
     return 0;
 }
